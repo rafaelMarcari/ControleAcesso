44fab5c5f2d72137c53bd8822853efe0ddc6a144
Rafael <marcari.rafael@gmail.com>
2015-06-14 18:39:19 -0300

    Criado a classe Arquivo

---
+++ src/main/java/br/edu/utfpr/model/Arquivo.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Rafael
+ */
+@Entity
+@Table(name = "tb_arquivo")
+public class Arquivo implements Serializable{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nomeArquivo;
+    
+    @ManyToOne
+    private Pasta pasta;
+
+    public Arquivo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeArquivo() {
+        return nomeArquivo;
+    }
+
+    public void setNomeArquivo(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public Pasta getPasta() {
+        return pasta;
+    }
+
+    public void setPasta(Pasta pasta) {
+        this.pasta = pasta;
+    }
+
+    @Override
+    public String toString() {
+        return "Arquivo{" + "nomeArquivo=" + nomeArquivo + ", pasta=" + pasta + '}';
+    }
+    
+}

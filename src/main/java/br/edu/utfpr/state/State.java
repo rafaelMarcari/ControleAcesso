0cbaaf07cf89e63903f0c63d9b2bae64036fd95b
Rafael <marcari.rafael@gmail.com>
2015-06-14 18:47:45 -0300

    Criado a interface State dentro do pacote state

---
+++ src/main/java/br/edu/utfpr/state/State.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.state;
+
+/**
+ *
+ * @author Rafael
+ */
+public interface State {
+    public String enviaMensagem();
+    public Boolean conectado();
+}

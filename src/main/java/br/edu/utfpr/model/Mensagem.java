c6ef4fbf930ce66fd191352208c7bb596c3bad02
Rafael <marcari.rafael@gmail.com>
2015-06-14 18:40:41 -0300

    Criado a classe Mensagem

---
+++ src/main/java/br/edu/utfpr/model/Mensagem.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Rafael
+ */
+@Entity
+@Table(name = "tb_mensagem")
+public class Mensagem implements Serializable{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+
+    public Mensagem() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensagem{" + "descricao=" + descricao + '}';
+    }
+
+}

c42e8037c114c284f4e2e5ecab8c1659bd4a38b5
Rafael <marcari.rafael@gmail.com>
2015-06-14 18:41:39 -0300

    Criado a classe Pasta

---
+++ src/main/java/br/edu/utfpr/model/Pasta.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.model;
+
+import br.edu.utfpr.proxy.IPasta;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Rafael
+ */
+@Entity
+@Table(name = "tb_pasta")
+public class Pasta implements IPasta, Serializable{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String nome;
+    private String path;
+    
+    @OneToMany(mappedBy = "pasta", targetEntity = Arquivo.class, cascade = CascadeType.ALL)
+    private List<Arquivo> arquivos;
+
+    public Pasta() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public List<Arquivo> getArquivos() {
+        return arquivos;
+    }
+
+    public void setArquivos(List<Arquivo> arquivos) {
+        this.arquivos = arquivos;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasta{" + "nome=" + nome + ", path=" + path + '}';
+    }
+
+    @Override
+    public String doSomething() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}

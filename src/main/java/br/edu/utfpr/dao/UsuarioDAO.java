373ffe3ea81b38cadd94b796327569739e6bfecd
Rafael <marcari.rafael@gmail.com>
2015-06-21 19:00:50 -0300

    Foi modificado o UsuarioDAO

--- src/main/java/br/edu/utfpr/dao/UsuarioDAO.java
+++ src/main/java/br/edu/utfpr/dao/UsuarioDAO.java
@@ -5,36 +5,17 @@
  */
 package br.edu.utfpr.dao;
 
-import javax.persistence.PersistenceException;
-import javax.persistence.criteria.CriteriaBuilder;
+import br.edu.utfpr.model.Usuario;
+import javax.persistence.EntityManager;
 
 /**
  *
  * @author Rafael
  */
-public class UsuarioDAO extends GenericDAO<Usuario>{
-    
-    public Usuario findByLogin(String login) {
-        Usuario u = null;
-        try {
-            getEntityManager().getTransaction().begin();
-            Criteria ct = getSession().createCriteria(Usuario.class);
-            ct.add("usuario", login);
-            u = (Usuario) ct.uniqueResult();
-        } catch (PersistenceException e) {
-            e.getMessage();
-        }
-        getEntityManager().getTransaction().commit();
-        return u;
-    }
-    
-    public List<Usuario> tudo() {
-        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<Usuario> cq = cb.createQuery(Usuario.class);
-        Root<Usuario> rootEntry = cq.from(Usuario.class);
-        CriteriaQuery<Usuario> all = cq.select(rootEntry);
-        TypedQuery<Usuario> allQuery = getEntityManager().createQuery(all);
-        return allQuery.getResultList();
+public class UsuarioDAO extends GenericDAO<Integer, Usuario>{
+
+    public UsuarioDAO(EntityManager em) {
+        super(em);
     }
     
 }
